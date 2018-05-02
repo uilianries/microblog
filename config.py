8a0cd7082d1436feeec3d63585772071d36078dd
Uilian Ries <uilianries@gmail.com>
2018-05-01 21:17:17 -0300

    Add migrate support
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- config.py
+++ config.py
@@ -3,5 +3,11 @@
 
 import os
 
+_BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '389rjwddefje892jf29eijf'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(_BASEDIR, 'app.db')
+    # do not signal my app for each new change on DB
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
