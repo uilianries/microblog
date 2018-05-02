0b6b22622c5e2622edee1916f3f6a7f56f0b773f
Uilian Ries <uilianries@gmail.com>
2018-05-01 22:46:43 -0300

    add user login
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,14 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
+# route user to login page when not authenticated
+login.login_view = 'login'
 
 from app import routes, models
