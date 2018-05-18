4f25139a64bab80d66bf3676cb469fc195a38e37
Uilian Ries <uilianries@gmail.com>
2018-05-18 20:22:14 -0300

    Log error
    - Add email support to errors
    - Add log support
    - Fix duplicated username on edit
    
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- config.py
+++ config.py
@@ -11,3 +11,11 @@ class Config(object):
         'sqlite:///' + os.path.join(_BASEDIR, 'app.db')
     # do not signal my app for each new change on DB
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # email
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['uilianries@gmail.com']
