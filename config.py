5be4ff991f7b294e157f2cd4577fc122bc5dbb61
Uilian Ries <uilianries@gmail.com>
2018-05-25 19:45:09 -0300

    part IX - pagination
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- config.py
+++ config.py
@@ -19,3 +19,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['uilianries@gmail.com']
+    POSTS_PER_PAGE = 3
--- tests.py
+++ tests.py
@@ -58,16 +58,15 @@ class UserModelCase(unittest.TestCase):
 
         # create four posts
         now = datetime.utcnow()
-        p1 = Post(body="post from john", user_id=u1.id,
+        p1 = Post(body="post from john", author=u1,
                   timestamp=now + timedelta(seconds=1))
-        p2 = Post(body="post from susan", user_id=u2.id,
+        p2 = Post(body="post from susan", author=u2,
                   timestamp=now + timedelta(seconds=4))
-        p3 = Post(body="post from mary", user_id=u3.id,
+        p3 = Post(body="post from mary", author=u3,
                   timestamp=now + timedelta(seconds=3))
-        p4 = Post(body="post from david", user_id=u4.id,
+        p4 = Post(body="post from david", author=u4,
                   timestamp=now + timedelta(seconds=2))
         db.session.add_all([p1, p2, p3, p4])
-        db.session.commit()
 
         # setup the followers
         u1.follow(u2)  # john follows susan
